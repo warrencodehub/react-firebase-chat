ca23c233e259bd1a27aceb4e748d6158899fb3ce
Miraflor <wmiraflor@hartmannph.com>
2024-06-28 10:16:06 +0800

    initial upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/firebase-react-chat/",
+  base: "/react-firebase-chat/",
   plugins: [react()],
 });
