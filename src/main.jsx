0044fa7e5f2854cdb0cdb2fa11d35108734c011d
Miraflor <wmiraflor@hartmannph.com>
2024-06-28 15:53:20 +0800

    test update

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/react-firebase-chat-app/">
-      <Routes>
-        <Route path="*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
